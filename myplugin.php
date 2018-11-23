346feb232a1e4eeafa295810269d57c436719c8e
Danielle Ali <dani.maria.ali@gmail.com>
2018-11-23 15:52:22 -0500

    Initial commit

---
+++ myplugin.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: My Plugin 
+Description: Example plugin for learning purposes
+Version: 1.0
+Author: Danielle Maria Ali
+Author URI: https://www.dmariali.com
+License: GPL v3.0+
+Text Domain: my-plugin
+*/ 
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+// if admin area
+if (is_admin()){
+
+    //include dependencies
+    require_once plugin_dir_path(__FILE__). 'admin/admin-menu.php'; 
+    require_once plugin_dir_path(__FILE__). 'admin/settings-page.php';
+    require_once plugin_dir_path(__FILE__). 'admin/settings-register.php';
+    require_once plugin_dir_path(__FILE__). 'admin/settings-callbacks.php'; 
+}
+
+//include dependencies: admin and public 
+require_once plugin_dir_path(__FILE__). 'includes/core-functions.php';
+
+function myplugin_options_default(){
+
+    return array(
+        'custom_url'    => 'https://wordpress.org', 
+        'custom_title'  => 'Powered by Haterade', 
+        'custom_style'  => 'disable', 
+        'custom_message'=> '<p class="custom-message"> My custom Message </p>', 
+        'custom_footer' => 'Special Message for users... so very special', 
+        'custom_toolbar'=> false, 
+        'custom_scheme' => 'default', 
+    );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
